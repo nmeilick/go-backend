41b9909c154d5b0d59541abd0b6213fe783d4930
nm <nm@ganyme.de>
2022-09-21 19:26:08 +0200

    Add File.Glob()

--- file.go
+++ file.go
@@ -3,6 +3,7 @@ package backend
 import (
 	"io"
 	"os"
+	"path/filepath"
 )
 
 // FileBackend allows accessing locally mounted filesystems
@@ -10,4 +11,8 @@ type FileBackend struct{}
 
 func (b *FileBackend) Open(path string) (io.ReadCloser, error) {
 	return os.Open(path)
+}
+
+func (b *FileBackend) Glob(pattern string) ([]string, error) {
+	return filepath.Glob(pattern)
 }
